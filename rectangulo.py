ef81e73877378fb71b0eb31f7d3833789acf72f6
lucas-mercado <lucasemercado21@gmail.com>
2022-08-15 23:12:42 +0000

    ejer rectangulo

---
+++ rectangulo.py
@@ -0,0 +1,32 @@
+"""
+Instrucciones de la tarea:
+- En el siguiente ejercicio se solicita calcular el área y el perímetro de un Rectángulo,
+para ello deberemos crear las siguientes variables:
+
+alto (int)
+ancho (int)
+
+- El usuario debe proporcionar los valores de alto y ancho,
+y después se debe imprimir el resultado en el siguiente formato
+(no usar acentos y respetar los espacios, mayúsculas, minúsculas y saltos de línea):
+
+Proporciona el alto:
+Proporciona el ancho:
+Area: <area>
+Perimetro: <perimetro>
+
+Las formulas para calcular el area y el perimetro de un Rectangulo son:
+Area: alto * ancho
+Perimetro: (alto + ancho) * 2
+"""
+
+alto=int(input('Proporcionar el alto: '))
+ancho=int(input('Proporcionar el ancho: '))
+
+area = alto * ancho
+perimetro= (alto + ancho ) * 2
+
+print('Area: ', area)
+print('Perimetro: ', perimetro)
+
+
